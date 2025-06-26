5042bfdace9fde59d23f2b151ec3ec058af2f9e4
Tanmoy_Dcc <tb@dccil.com>
2025-06-26 15:54:22 +0530

    multiple git account commit check

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -37,7 +37,7 @@ export default function Index() {
 
 
   return (  
-    <View className="flex-1 bg-primary">
+    <View className="flex-1 bg-primary">  
       <Image source={images.bg} className="absolute w-full z-0" />
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
