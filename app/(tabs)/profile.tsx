1629147a5f221ea9283f1b5d4ff4d4130f9f6cbe
Tanmoy_Dcc <tb@dccil.com>
2025-06-26 11:33:10 +0530

    add profile page and save page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { icons } from '@/constants/icons'
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col gap-5'>
+        <Image source={icons.arrow} className='size-10'
+          tintColor="#Fff" />
+        <Text className='text-gray-500 text-base'></Text>
+      </View>
     </View>
   )
 }
-
 export default profile
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { icons } from '@/constants/icons'
 
 const saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col gap-5'>
+        <Image source={icons.save} className='size-10'
+          tintColor="#Fff" />
+        <Text className='text-gray-500 text-base'></Text>
+      </View>
     </View>
   )
 }
